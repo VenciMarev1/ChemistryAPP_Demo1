90ae744b0e19761a60d8c69839d5de87d10e5663
VenciMarev <vencislavmarev21a@gmail.com>
2025-02-16 15:58:11 +0200

    changes

--- ChemistryApp/Assets/Scripts/InstantiateAtom.cs
+++ ChemistryApp/Assets/Scripts/InstantiateAtom.cs
@@ -5,6 +5,7 @@ using TMPro;
 using System;
 using System.Reflection;
 using System.Xml.Linq;
+using System.Linq;
 
 public class InstantiateAtom : MonoBehaviour
 {
@@ -14,9 +15,7 @@ public class InstantiateAtom : MonoBehaviour
     public List<GameObject> NonMetals;
 
     public TMP_Text text;
-
-    int Anion;
-    int Ion;
+    List<Atom> atoms_Script;
 
     public TMP_InputField Input;
     // Start is called before the first frame update
@@ -54,7 +53,7 @@ public class InstantiateAtom : MonoBehaviour
     #region
     //Tva nai veroqtno shte trqbva da e v otdelen cs fail.
 
-    public void SolveChemEqation()
+    public string SolveChemEqation(int Ion, int Anion)
     {
         List<GameObject> Atoms = new List<GameObject>();
         Dictionary<string, Dictionary<int, int>> AtomDict = new Dictionary<string, Dictionary<int, int>>();
@@ -87,14 +86,15 @@ public class InstantiateAtom : MonoBehaviour
             AtomScript.Add(Atoms[i].GetComponent<Atom>());
         }
 
-        if (AtomScript.Count > 1)
+        if (AtomScript.Count >= 1)
         {
-            SortAtoms(AtomScript, AtomDict);
+            return SortAtoms(AtomScript, AtomDict);
         }
+        else return "Null";
     }
 
 
-    private void SortAtoms(List<Atom> atomScript, Dictionary<string, Dictionary<int, int>> AtomDict)
+    private string SortAtoms(List<Atom> atomScript, Dictionary<string, Dictionary<int, int>> AtomDict)
     {
         /* for (int i = 0; i < atomScript.Count; i++)
          {
@@ -109,10 +109,10 @@ public class InstantiateAtom : MonoBehaviour
              }
          }*/
 
-        ElementsLogic(atomScript, AtomDict);
+        return ElementsLogic(atomScript, AtomDict);
     }
 
-    private void ElementsLogic(List<Atom> atomScript, Dictionary<string, Dictionary<int,int>> AtomDict)
+    private string ElementsLogic(List<Atom> atomScript, Dictionary<string, Dictionary<int,int>> AtomDict)
     {
         //preemption of the electrons/valency
         List<int> electrons = new List<int>();
@@ -142,12 +142,13 @@ public class InstantiateAtom : MonoBehaviour
         }
 
 
-        DisplayElement(AtomDict, atomScript);
+       return DisplayElement(AtomDict, atomScript);
 
     }
 
-    private void DisplayElement(Dictionary<string, Dictionary<int, int>> AtomDict, List<Atom> pNames)
+    private string DisplayElement(Dictionary<string, Dictionary<int, int>> AtomDict, List<Atom> pNames)
     {
+        string Text = null;
         List<string> ElementsWithIndex = new List<string>() { "OH", "SO<sub>3</sub>", "SO<sub>4</sub>", "NO<sub>3</sub>" };
         List<string> mathc = new List<string>();
         //Da se napravi da gi podrejda pravilno s indeksi s poveche ot 2 elementa.
@@ -184,11 +185,11 @@ public class InstantiateAtom : MonoBehaviour
                 {
                     names.Add(getSpeacialName(a.Key, pNames));
                 }
-                else if (b.Value != 1 && b.Key < 0 && getSpecialAtom(ElementsWithIndex, a.Key))
+                else if (b.Value != 1 && b.Key < 0 && !getSpecialAtom(ElementsWithIndex, a.Key))
                 {
                     names.Add(getSpeacialName(a.Key, pNames) + $"<sub>{b.Value}</sub>");
                 }
-                else if (b.Value != 1 && b.Key < 0 && !getSpecialAtom(ElementsWithIndex, a.Key))
+                else if (b.Value != 1 && b.Key < 0 && getSpecialAtom(ElementsWithIndex, a.Key))
                 {
                     names.Add("(" + getSpeacialName(a.Key, pNames) + ")" + $"<sub>{b.Value}</sub>");
                 }
@@ -198,9 +199,12 @@ public class InstantiateAtom : MonoBehaviour
 
         foreach (var a in names)
         {
-            text.text += a;
+            Text += a;
         }
-        Debug.Log(text.text);
+
+        Debug.Log(Text);
+
+        return Text;
 
         /* foreach (var a in AtomDict)
          {
@@ -245,7 +249,7 @@ public class InstantiateAtom : MonoBehaviour
         {
             if (el == a.Name)
             {
-                return a.NameForPresentation;
+                return a.NameForPresentation + $"<sup>{a.Electrons}</sup>";
             }
         }
         return "No Matches";
@@ -334,8 +338,9 @@ public class InstantiateAtom : MonoBehaviour
         string[] atoms = arg0.Split(things);
 
         List<string> FindAtoms = new List<string>();
+        atoms_Script = new List<Atom>();
 
-        for(int i = 0; i < Positive_ElementsEntiPyt.Count; i++)
+        for (int i = 0; i < Positive_ElementsEntiPyt.Count; i++)
         {
             if (atoms[0].Contains(Positive_ElementsEntiPyt[i]))
             {
@@ -374,14 +379,17 @@ public class InstantiateAtom : MonoBehaviour
         }
         //The findAtoms list should look like this metal, nonmetal, metal
 
-        CheckElementsMetals(FindAtoms[2]);
-        CheckElementsNonMetals(FindAtoms[1]);
-
         
+        //CheckElementsMetals(FindAtoms[0]);
+
+        //CheckElementsMetals(FindAtoms[2]);
+        //CheckElementsNonMetals(FindAtoms[1]);
+
+        SomeLogic(FindAtoms);
 
         List<string> equations = new List<string>();
-        equations.Add(CreateEquation());
-        text.text += $" + {FindAtoms[0]}";
+        //equations.Add(CreateEquation());
+       // text.text += $" + {FindAtoms[0]}";
         /*foreach (var a in FindAtoms)
         {
             CheckElementsMetals(a);
@@ -390,35 +398,271 @@ public class InstantiateAtom : MonoBehaviour
         }*/
     }
 
-    public void CheckElementsMetals(string Element)
+    public int CheckElementsMetals(string Element)
     {
         for(int i = 0; i < Metals.Count; i ++)
         {
             if (Metals[i].GetComponent<Atom>().Name == Element)
             {
-                Ion = i;
+                atoms_Script.Add(Metals[i].GetComponent<Atom>());
+                return i;
             }
         }
+        return -1;
     }
 
-    public void CheckElementsNonMetals(string Element)
+    public int CheckElementsNonMetals(string Element)
     {
         for (int i = 0; i < NonMetals.Count; i++)
         {
             if (NonMetals[i].GetComponent<Atom>().Name == Element)
             {
-                Anion = i;
+                atoms_Script.Add(NonMetals[i].GetComponent<Atom>());
+                return i;
             }
         }
+        return -1;
     }
 
-    public string CreateEquation()
+    public string CreateEquation()//not used rn
     {
-        SolveChemEqation();
+        //SolveChemEqation();
 
         return null;
     }
 
+    private void SomeLogic(List<string> FindAtoms)
+    {
+        
+        string a = SolveChemEqation(CheckElementsMetals(FindAtoms[0]),CheckElementsNonMetals(FindAtoms[1]));
+        string b = SolveChemEqation(CheckElementsMetals(FindAtoms[2]), CheckElementsNonMetals(FindAtoms[1]));
+
+        List<int> el = new List<int>();
+        foreach(var c in atoms_Script)
+        {
+            el.Add(c.Electrons);
+        }
+
+        string LeftSide = $"{a} + {atoms_Script[2].NameForPresentation}<sup>0</sup>";
+        string RightSide =  $"-> {b} + {atoms_Script[0].NameForPresentation}<sup>0</sup>";
+
+        //ChemicalEquationBalancer ch = new ChemicalEquationBalancer(text);
+        //ch.Solve(e);
+        //text.text = e;
+    }
+
+    void BalancingSide()
+    {
+    }
+
+
+    //for balancing both sides need to be equal
+
+
+
 
 }
+
+#region 
+class ChemicalEquationBalancer
+{
+    public TMP_Text tMP_Text { get; set; }
+
+    public ChemicalEquationBalancer(TMP_Text tMP_Text)
+    {
+        this.tMP_Text = tMP_Text;
+    }
+    public void Solve(string equation)
+    {
+        try
+        {
+            BalanceEquation(equation);
+        }
+        catch (Exception ex)
+        {
+            Debug.Log($"Error: {ex.Message}");
+        }
+    }
+
+    void BalanceEquation(string equation)
+    {
+        var sides = equation.Split("->", StringSplitOptions.RemoveEmptyEntries);
+        if (sides.Length != 2)
+            throw new Exception("Invalid equation format. Use 'reactants -> products'.");
+
+        var reactants = ParseCompounds(sides[0]);
+        var products = ParseCompounds(sides[1]);
+
+        var allElements = reactants.SelectMany(c => c.ElementCounts.Keys)
+                                   .Union(products.SelectMany(c => c.ElementCounts.Keys))
+                                   .Distinct()
+                                   .ToList();
+
+        var coefficients = Balance(reactants, products, allElements);
+
+        PrintBalancedEquation(reactants, products, coefficients);
+    }
+
+    static List<ChemicalCompound> ParseCompounds(string side)
+    {
+        return side.Split('+', StringSplitOptions.RemoveEmptyEntries)
+                   .Select(compound => new ChemicalCompound(compound.Trim()))
+                   .ToList();
+    }
+
+    static int[] Balance(List<ChemicalCompound> reactants, List<ChemicalCompound> products, List<string> allElements)
+    {
+        int n = reactants.Count + products.Count;
+        int m = allElements.Count;
+
+        int[,] matrix = new int[m, n];
+
+        for (int i = 0; i < reactants.Count; i++)
+        {
+            foreach (var kvp in reactants[i].ElementCounts)
+            {
+                int elementIndex = allElements.IndexOf(kvp.Key);
+                matrix[elementIndex, i] = kvp.Value;
+            }
+        }
+
+        for (int i = 0; i < products.Count; i++)
+        {
+            foreach (var kvp in products[i].ElementCounts)
+            {
+                int elementIndex = allElements.IndexOf(kvp.Key);
+                matrix[elementIndex, reactants.Count + i] = -kvp.Value;
+            }
+        }
+
+        int[] coefficients = SolveLinearEquations(matrix);
+        return coefficients;
+    }
+
+    static int[] SolveLinearEquations(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+
+        int[] solution = new int[cols];
+        for (int i = 0; i < cols; i++) solution[i] = 1; // Default coefficients
+
+        for (int row = 0; row < rows; row++)
+        {
+            int gcd = 0;
+            for (int col = 0; col < cols; col++)
+            {
+                gcd = GCD(gcd, Math.Abs(matrix[row, col]));
+            }
+
+            if (gcd > 0)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] /= gcd;
+                }
+            }
+        }
+
+        for (int col = 0; col < cols; col++)
+        {
+            solution[col] = 1;
+            for (int row = 0; row < rows; row++)
+            {
+                if (matrix[row, col] != 0)
+                {
+                    solution[col] *= Math.Abs(matrix[row, col]);
+                }
+            }
+        }
+
+        int lcm = 1;
+        foreach (int coef in solution)
+        {
+            lcm = LCM(lcm, coef);
+        }
+
+        for (int i = 0; i < cols; i++)
+        {
+            solution[i] = solution[i] * lcm;
+        }
+
+        return solution;
+    }
+
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static int LCM(int a, int b)
+    {
+        return (a / GCD(a, b)) * b;
+    }
+
+    void PrintBalancedEquation(List<ChemicalCompound> reactants, List<ChemicalCompound> products, int[] coefficients)
+    {
+        var reactantStrings = reactants.Select((compound, index) => $"{coefficients[index]} {compound.Formula}").ToList();
+        var productStrings = products.Select((compound, index) => $"{coefficients[reactants.Count + index]} {compound.Formula}").ToList();
+
+        tMP_Text.text = string.Join(" + ", reactantStrings) + " -> " + string.Join(" + ", productStrings);
+    }
+}
+class ChemicalCompound
+{
+    public string Formula { get; private set; }
+    public Dictionary<string, int> ElementCounts { get; private set; }
+
+    public ChemicalCompound(string formula)
+    {
+        Formula = formula;
+        ElementCounts = ParseElementCounts(formula);
+    }
+
+    private Dictionary<string, int> ParseElementCounts(string formula)
+    {
+        var elementCounts = new Dictionary<string, int>();
+        string element = "";
+        int count = 0;
+
+        for (int i = 0; i < formula.Length; i++)
+        {
+            char c = formula[i];
+
+            if (char.IsUpper(c))
+            {
+                if (!string.IsNullOrEmpty(element))
+                {
+                    elementCounts[element] = count == 0 ? 1 : count;
+                }
+
+                element = c.ToString();
+                count = 0;
+            }
+            else if (char.IsLower(c))
+            {
+                element += c;
+            }
+            else if (char.IsDigit(c))
+            {
+                count = count * 10 + (c - '0');
+            }
+        }
+
+        if (!string.IsNullOrEmpty(element))
+        {
+            elementCounts[element] = count == 0 ? 1 : count;
+        }
+
+        return elementCounts;
+    }
+}
+#endregion
+
 //1:54 :,( last edited
